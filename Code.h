6d53e12191c0a9b46fd4437b210f47b18bc5c450
andeysiniy <149255559+romachenkodaria@users.noreply.github.com>
2023-11-25 01:49:22 +0300

    Добавлена документация для doxygen в Code

--- Code.h
+++ Code.h
@@ -1,25 +1,49 @@
+/*!
+*\file
+*\brief  В данном файле находятся заголовки функций и поля класса Code, которые используются для работы программы.
+*/
 #pragma once
 #include <vector>
 #include <string>
 #include "CodeString.h"
 
+
+/*!
+* \brief Класс описывающий код как вектор объектов строк кода CodeString
+* \param codeStrings - строки кода
+*/
 class Code {
 public:
-    std::vector<CodeString> codeStrings;
+	std::vector<CodeString> codeStrings;
 
-    Code(const std::vector<std::string>& lines) {
-        for (const std::string& line : lines) {
-            codeStrings.emplace_back(line.c_str());
-        }
-    }
+	Code(const std::vector<std::string>& lines) {
+		for (const std::string& line : lines) {
+			codeStrings.emplace_back(line.c_str());
+		}
+	}
 
+	/*!
+	* \brief Функция для нахождения индекса строки со вхождением правильного открытия многострочного комментария
+	* \return Индекс вхождения, -1, если не найден
+	*/
+	int commentOpeningPos();
 
-    int commentOpeningPos();
+	/*!
+	* \brief Функция для нахождения индекса строки со вхождением правильного закрытия многострочного комментария
+	* \return Индекс вхождения, -1, если не найден
+	*/
+	int commentClosingPos();
 
-    int commentClosingPos();
+	/*!
+	* \brief Функция удаляет многострочный комментарий с заранее известными индексами строк начала и конца комментария.
+	* \param start - индекс строки с правильным открытием многострочного комментария.
+	* \param end - индекс строки с правильным закрытием многострочного комментария.
+	*/
+	void removeComment(int start, int end);
 
-    void removeComment(int start, int end);
-
-    void removeMultiLineComments();
+	/*!
+	* \brief Главная вычислительная функция программы, удаляющая все многострочные комментарии в коде.
+	*/
+	void removeMultiLineComments();
 };
 
