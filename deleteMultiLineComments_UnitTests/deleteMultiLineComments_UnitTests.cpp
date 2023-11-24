8f36c66978dda2711b8f7d4a440794eae949abdc
andeysiniy <149255559+romachenkodaria@users.noreply.github.com>
2023-11-24 23:10:33 +0300

    Добавлен проект Unit тестирования

---
+++ deleteMultiLineComments_UnitTests/deleteMultiLineComments_UnitTests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Code.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace deleteMultiLineCommentsUnitTests
+{
+	TEST_CLASS(deleteMultiLineCommentsUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
