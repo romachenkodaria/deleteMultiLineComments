eef81496629c017103713c10c7b62ac7128555dc
andeysiniy <149255559+romachenkodaria@users.noreply.github.com>
2023-11-25 01:43:08 +0300

    Добавлена документация для doxygen в CodeString

--- CodeString.h
+++ CodeString.h
@@ -1,30 +1,52 @@
+/*!
+*\file
+*\brief  В данном файле находятся заголовки функций и поля класса CodeString, которые используются для работы программы.
+*/
 #pragma once
 #include <string>
 #include <regex>
 
+/*!
+* \brief Класс описывающий строку кода и вхождения спец. символов в строке
+* \param text - текст строки кода
+* \param singleLineCommentPosition - индекс вхождения начала однострочного комментария
+* \param multiLineCommentStartPosition - индекс позиции начала многострочного комментария
+* \param multiLineCommentEndPosition - индекс позиции конца многострочного комментария
+* \param stringStartLiteralPosition - индекс позиции начала строкового литерала
+* \param stringEndLiteralPosition - индекс позиции конца строкового литерала
+*/
 class CodeString
 {
 public:
-    std::string text;
-    int singleLineCommentPosition;
-    int multiLineCommentStartPosition;
-    int multiLineCommentEndPosition;
-    int stringStartLiteralPosition;
-    int stringEndLiteralPosition;
+	std::string text;
+	int singleLineCommentPosition;
+	int multiLineCommentStartPosition;
+	int multiLineCommentEndPosition;
+	int stringStartLiteralPosition;
+	int stringEndLiteralPosition;
 
-    CodeString(const std::string& text) : text(text),
-        singleLineCommentPosition(-1),
-        multiLineCommentStartPosition(-1),
-        multiLineCommentEndPosition(-1),
-        stringStartLiteralPosition(-1),
-        stringEndLiteralPosition(-1) {
-        extractCommentsAndStrings();
-    } 
+	CodeString(const std::string& text) : text(text),
+		singleLineCommentPosition(-1),
+		multiLineCommentStartPosition(-1),
+		multiLineCommentEndPosition(-1),
+		stringStartLiteralPosition(-1),
+		stringEndLiteralPosition(-1) {
+		extractCommentsAndStrings();
+	}
 
-    void extractCommentsAndStrings();
-
-    bool containsStartOfMultiLineComment();
-
-    bool containsEndOfMultiLineComment();
+	/*!
+	* \brief Функция для создания полей класса CodeString с индексами вхождений специальных символов.
+	*/
+	void extractCommentsAndStrings();
+	/*!
+	* \brief Функция для проверки строки на наличие правильного вхождения символов открытия многострочного комментария.
+	* \return Есть ли в строке правильное вхождение начала комментария
+	*/
+	bool containsStartOfMultiLineComment();
+	/*!
+	* \brief Функция для проверки строки на наличие правильного вхождения символов закрытия многострочного комментария.
+	* \return Есть ли в строке правильное вхождение закрытия комментария
+	*/
+	bool containsEndOfMultiLineComment();
 };
 
