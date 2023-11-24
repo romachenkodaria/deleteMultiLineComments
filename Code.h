cd738d6847e46651d5518b3a2342314ae1c2cd2a
andeysiniy <149255559+romachenkodaria@users.noreply.github.com>
2023-11-24 20:48:38 +0300

    Добавлен класс Code

---
+++ Code.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include "CodeString.h"
+
+class Code {
+public:
+    std::vector<CodeString> codeStrings;
+
+    Code(const std::vector<std::string>& lines) {
+        for (const std::string& line : lines) {
+            codeStrings.emplace_back(line.c_str());
+        }
+    }
+
+    // TODO: функция определения начала/конца комментария
+
+
+};
+
